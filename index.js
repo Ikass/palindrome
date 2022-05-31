cc311272bcbf041d7700c5ca09cf83fd328bcc91
Reinis Ikass <reinis.ikass@gmail.com>
2022-05-31 12:17:19 +0300

    add reamde

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-// reverse a string
-function reverse(string) {
-    // does not work with emojis
-    // return string.split("").reverse().join("");
-    // works with emojis
-    return Array.from(string).reverse().join("");
-}
-
-// returns true for palindrome, false otherwise
-// function palindrome(string) {
-//     let processedContent = string.toLowerCase();
-//     return processedContent === reverse(processedContent);
-// }
-
-/*******************
- Exercise
-******************/
-// return array of username and domain from email address
-function emailParts(email) {
-    return email.toLowerCase().split("@");
-}
+module.exports = Phrase;
 
 // Add `reverse` to all strings.
 String.prototype.reverse = function() {
     return Array.from(this).reverse().join("");
-}
-
-// Exercise:
-// Add `blank` to all strings to check for empty strings
-// had to google for ideas
-// https://bobbyhadz.com/blog/javascript-check-if-string-contains-only-spaces
-String.prototype.blank = function() {
-    // return this === this.match(/^\s*$/g);
-    return this.length === 0 || /^\s*$/g.test(this);
-    // return this.split(/^\s*$/g) !== 0;
-}
-
-// Exercise:
-// Add a `last` method to Array prototype to return the last element
-Array.prototype.last = function() {
-    return this.slice(-1);
 }
 
 // Defines a Phrase object.
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Phrase object w/ palindrome detector
+
+Built as part of the [Learn Enough JavaScript to be Dangerous](https://www.learnenough.com/javascript) tutorial.
